748d043ac8841ea5f7c53740d11505d65fb33a56
calvinwins <weaver95@byui.edu>
2022-02-17 19:30:56 -0600

    no move up and down

--- Greed/game/services/keyboard_service.py
+++ Greed/game/services/keyboard_service.py
@@ -27,7 +27,7 @@ class KeyboardService:
             Point: The selected direction.
         """
         dx = 0
-        dy = 0
+        # dy = 0
 
         if pyray.is_key_down(pyray.KEY_LEFT):
             dx = -1
@@ -35,11 +35,11 @@ class KeyboardService:
         if pyray.is_key_down(pyray.KEY_RIGHT):
             dx = 1
         
-        if pyray.is_key_down(pyray.KEY_UP):
-            dy = -1
+        # if pyray.is_key_down(pyray.KEY_UP):
+        #     dy = -1
         
-        if pyray.is_key_down(pyray.KEY_DOWN):
-            dy = 1
+        # if pyray.is_key_down(pyray.KEY_DOWN):
+        #     dy = 1
 
         direction = Point(dx, dy)
         direction = direction.scale(self._cell_size)
